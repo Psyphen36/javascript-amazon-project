448f33ec9980fe38328e2e38fb619f83891406e1
psyphen36 <Anupammishra87706@gmail.com>
2024-02-16 17:00:20 +0530

    creating cart quantity and displaying on the web by making it interactive

---
+++ scripts/script.js
@@ -0,0 +1,81 @@
+
+let HTMLData = '';
+
+products.forEach((product) => {
+  HTMLData += `<div class="product-container">
+    <div class="product-image-container">
+      <img class="product-image"
+        src="${product.image}">
+    </div>
+
+    <div class="product-name limit-text-to-2-lines">
+    ${product.name}
+    </div>
+
+    <div class="product-rating-container">
+      <img class="product-rating-stars"
+        src="images/ratings/rating-${product.rating.stars * 10}.png">
+      <div class="product-rating-count link-primary">
+        ${product.rating.count}
+      </div>
+    </div>
+
+    <div class="product-price">
+      $${product.priceCents / 100}
+    </div>
+
+    <div class="product-quantity-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+      </select>
+    </div>
+
+    <div class="product-spacer"></div>
+
+    <div class="added-to-cart">
+      <img src="images/icons/checkmark.png">
+      Added
+    </div>
+
+    <button class="add-to-cart-button button-primary js-addCart"
+    data-product-id="${product.id}">
+      Add to Cart
+    </button>
+  </div>
+`
+});
+document.querySelector('.js-grid-container').innerHTML = HTMLData;
+
+document.querySelectorAll('.js-addCart').forEach((buttons) => {
+  buttons.addEventListener('click', () => {
+    const productId = buttons.dataset.productId;
+    let matchingItem;
+
+    cart.forEach(item => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+      });
+
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else{
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+
+    let cartQuantity = 
+    console.log('cart items',cart);
+  })
+})
